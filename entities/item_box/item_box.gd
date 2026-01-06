502500352aebec063b61f87ef71cffc19786a287
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-01-06 10:57:17 +0000

    Started working on an random item box system

---
+++ entities/item_box/item_box.gd
@@ -0,0 +1,60 @@
+extends Node3D
+
+@export var _items: Array[ItemResource] = []
+@export var _disabled_duration: float = 5.0
+@export var _mesh_root: Node3D
+@export var _disabled_timer: Timer
+
+var _enabled: bool = true
+var _time: float = 0.0
+
+
+func _ready() -> void:
+	_disabled_timer.wait_time = _disabled_duration
+
+
+func _process(delta: float) -> void:
+	# Make the box move up and down
+	_time += delta
+	_mesh_root.position.y = 1.0 + sin(_time)
+
+
+func _give_car_random_item(_car: CarController) -> void:
+	# Get the total rarity of all items
+	var rarity_sum: int = 0
+	for item: ItemResource in _items:
+		rarity_sum += item.rarity
+	
+	# Get the random value that the item is at
+	var item_val: int = randi_range(0, rarity_sum)
+	
+	print(item_val)
+	
+	var item_sum: int = 0
+	for item: ItemResource in _items:
+		item_sum += item.rarity
+		if item_sum >= item_val:
+			print(item.name)
+			return
+
+
+func _disable_box() -> void:
+	_enabled = false
+	_mesh_root.hide()
+	_disabled_timer.start()
+
+
+func _enable_box() -> void:
+	_enabled = true
+	_mesh_root.show()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	# Check if the body is a car controller
+	if _enabled and body is CarController:
+		_give_car_random_item(body)
+		_disable_box()
+
+
+func _on_disabled_timer_timeout() -> void:
+	_enable_box()
---
+++ entities/items/item_resource.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name ItemResource
+
+
+@export var name: String = "Item"
+@export var icon: Texture2D
+@export var rarity: int = 1
+@export var scene: PackedScene
