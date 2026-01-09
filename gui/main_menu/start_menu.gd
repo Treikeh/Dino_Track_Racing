7572837bdb662e138327c05ecda8f0f7bf944dd4
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-01-09 11:39:20 +0000

    Added a version label to the main menu

--- gui/main_menu/start_menu.gd
+++ gui/main_menu/start_menu.gd
@@ -5,10 +5,12 @@ signal start_pressed
 
 
 @export var _start_button: Button
+@export var _version_label: Label
 
 
 func _ready() -> void:
 	_start_button.grab_focus.call_deferred()
+	_version_label.text = "v%s" % ProjectSettings.get_setting("application/config/version")
 
 
 func _on_play_button_pressed() -> void:
