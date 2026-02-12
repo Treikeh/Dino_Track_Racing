9cf4208b67ee60565c1af31001edc8a854938c1a
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-02-12 15:50:23 +0000

    Started working on the settings menu

---
+++ common/autoload/settings_manager.gd
@@ -0,0 +1,164 @@
+extends Node
+
+signal video_settings_changed
+signal audio_settings_changed
+signal gameplay_settings_changed
+
+
+enum DISPLAY_MODE {
+	FULLSCREEN,
+	BORDERLESS_FULLSCREEN,
+	WINDOWED,
+	BORDERLESS_WINDOWED,
+}
+
+
+const USER_PATH: String = "user://"
+const DEBUG_PATH: String = "res://debug/"
+
+
+const FILE_NAME: String = "settings.ini"
+const DEFAULTS: Dictionary = {
+	"AUDIO": {
+		"MASTER_VOLUME": 0.75,
+		"EFFECTS_VOLUME": 1.0,
+		"MUSIC_VOLUME": 1.0,
+	},
+	"VIDEO": {
+		"DISPLAY_MODE": DISPLAY_MODE.BORDERLESS_FULLSCREEN,
+		"VSYNC_MODE": DisplayServer.VSYNC_ENABLED,
+		"MAX_FPS": 60.0,
+	},
+	"GAMEPLAY": {
+		"ALLOW_CPUS": true,
+		"MIN_CAR_AMOUNT": 12,
+	},
+}
+
+var _config_file: ConfigFile = ConfigFile.new()
+
+@onready var _config_path: String = get_data_dir_path() + FILE_NAME
+
+
+func _ready() -> void:
+	if not FileAccess.file_exists(_config_path):
+		# Create new config file with all the settings.
+		#NOTE: Remember to delete settings.ini file when adding/removing elements
+		for section: String in DEFAULTS:
+			for setting: String in DEFAULTS[section]:
+				_config_file.set_value(section, setting.to_lower(), DEFAULTS[section][setting])
+		
+		save_settings()
+	else:
+		# Load config file
+		_config_file.load(_config_path)
+	
+	# Apply settings when the game starts
+	apply_audio_settings()
+	apply_video_settings()
+	apply_gameplay_settings()
+
+
+func save_settings() -> void:
+	_config_file.save(_config_path)
+
+
+func get_data_dir_path() -> String:
+	return DEBUG_PATH if OS.is_debug_build() else USER_PATH
+
+
+#region Audio
+
+func set_audio_setting(key: String, value) -> void:
+	_config_file.set_value("AUDIO", key, value)
+	audio_settings_changed.emit()
+
+
+func load_audio_settings() -> Dictionary:
+	var audio_settings: Dictionary = {}
+	for key in _config_file.get_section_keys("AUDIO"):
+		audio_settings[key] = _config_file.get_value("AUDIO", key)
+	return audio_settings
+
+
+func apply_audio_settings() -> void:
+	var audio_settings: Dictionary = load_audio_settings()
+	if audio_settings.is_empty():
+		return
+	
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), audio_settings.master_volume)
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Effects"), audio_settings.effects_volume)
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Music"), audio_settings.music_volume)
+
+#endregion
+
+
+#region Video
+
+func set_video_setting(key: String, value) -> void:
+	_config_file.set_value("VIDEO", key, value)
+	video_settings_changed.emit()
+
+
+func load_video_settings() -> Dictionary:
+	var video_settings: Dictionary = {}
+	for key in _config_file.get_section_keys("VIDEO"):
+		video_settings[key] = _config_file.get_value("VIDEO", key)
+	return video_settings
+
+
+func apply_video_settings() -> void:
+	var video_settings: Dictionary = load_video_settings()
+	if video_settings.is_empty():
+		return
+	
+	set_display_mode(video_settings.display_mode)
+	set_vsync_mode(video_settings.vsync_mode)
+	Engine.max_fps = int(video_settings.max_fps)
+
+
+func set_display_mode(display_mode: DISPLAY_MODE) -> void:
+	match display_mode:
+		DISPLAY_MODE.FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		DISPLAY_MODE.BORDERLESS_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+		DISPLAY_MODE.WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		DISPLAY_MODE.BORDERLESS_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+
+
+func set_vsync_mode(vsync_mode: DisplayServer.VSyncMode) -> void:
+	DisplayServer.window_set_vsync_mode(vsync_mode)
+
+#endregion
+
+
+#region Gameplay
+
+func set_gameplay_setting(key: String, value) -> void:
+	_config_file.set_value("GAMEPLAY", key, value)
+	gameplay_settings_changed.emit()
+
+
+func load_gameplay_settings() -> Dictionary:
+	var gameplay_settings: Dictionary = {}
+	for key in _config_file.get_section_keys("GAMEPLAY"):
+		gameplay_settings[key] = _config_file.get_value("GAMEPLAY", key)
+	return gameplay_settings
+
+
+func apply_gameplay_settings() -> void:
+	var gameplay_settings: Dictionary = load_gameplay_settings()
+	if gameplay_settings.is_empty():
+		return
+	
+	Globals.allow_cpus = gameplay_settings.allow_cpus
+	Globals.min_cars_ammount = gameplay_settings.min_car_amount
+
+#endregion
--- gui/main_menu/options_menu/options_menu.gd
+++ gui/main_menu/options_menu/options_menu.gd
@@ -4,12 +4,239 @@ extends Control
 signal menu_closed(menu: Control)
 
 
+@onready var _apply_button: Button = %ApplyButton
+
+
+func _ready() -> void:
+	_setup_audio_settings()
+	_setup_video_settings()
+	_setup_gameplay_settings()
+	_setup_confirm_pop_up()
+	
+	## Connect signals
+	_apply_button.pressed.connect(_on_apply_button_pressed)
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		close_menu()
+		_on_back_button_pressed()
+	
+	if event.is_action_pressed("ui_defaults"):
+		_on_defaults_button_pressed()
+
+
+func _on_back_button_pressed() -> void:
+	if !_are_new_and_old_settings_matching():
+		_confirm_pop_up.show()
+		#TODO: Make the confirm pop up the target focus when going back
+		return
+	
+	_close_settings_menu()
+
+
+func _close_settings_menu() -> void:
+	# Actually apply the settings when closing the menu. If the new settings aren't applied with ->
+	# <- the apply button then the new settings are discarded
+	SettingsManager.apply_audio_settings()
+	SettingsManager.apply_video_settings()
+	SettingsManager.apply_gameplay_settings()
+	close_menu()
 
 
 func close_menu() -> void:
 	hide()
 	menu_closed.emit(self)
 	process_mode = Node.PROCESS_MODE_DISABLED
+
+
+func _on_defaults_button_pressed() -> void:
+	var defaults: Dictionary = SettingsManager.DEFAULTS
+	_on_master_volume_changed(defaults.AUDIO.MASTER_VOLUME)
+	_on_display_mode_changed(defaults.VIDEO.DISPLAY_MODE)
+	_on_vsync_mode_changed(defaults.VIDEO.VSYNC_MODE)
+	_on_fps_changed(defaults.VIDEO.MAX_FPS)
+
+
+func _on_apply_button_pressed() -> void:
+	for setting: String in _new_audio_settings:
+		SettingsManager.set_audio_setting(setting, _new_audio_settings[setting])
+		
+	for setting: String in _new_video_settings:
+		SettingsManager.set_video_setting(setting, _new_video_settings[setting])
+	
+	for setting: String in _new_gameplay_settings:
+		SettingsManager.set_gameplay_setting(setting, _new_gameplay_settings[setting])
+	
+	SettingsManager.save_settings()
+	
+	# Reset apply button
+	_old_audio_settings = _new_audio_settings.duplicate()
+	_old_video_settings = _new_video_settings.duplicate()
+	_old_gameplay_settings = _new_gameplay_settings.duplicate()
+	_apply_button.disabled = true
+
+
+func _are_new_and_old_settings_matching() -> bool:
+	var audio: bool = _new_audio_settings == _old_audio_settings
+	var video: bool = _new_video_settings == _old_video_settings
+	var gameplay: bool = _new_gameplay_settings == _old_gameplay_settings
+	return (video and audio and gameplay)
+
+
+
+#region Confirm pop up
+
+@export_group("Confirm pop up")
+@export var _confirm_pop_up: PanelContainer
+@export var _save_button: Button
+@export var _discard_button: Button
+
+
+func _setup_confirm_pop_up() -> void:
+	_confirm_pop_up.hide()
+	_save_button.pressed.connect(_on_save_button_pressed)
+	_discard_button.pressed.connect(_on_discard_button_pressed)
+
+
+func _on_save_button_pressed() -> void:
+	_on_apply_button_pressed()
+	_close_settings_menu()
+
+
+func _on_discard_button_pressed() -> void:
+	_close_settings_menu()
+
+#endregion
+
+
+
+#region Audio settings
+
+@export_group("Audio")
+@export var _master_volume_slider: HSlider
+@export var _master_volume_value: Label
+@export var _effects_volume_slider: HSlider
+@export var _effects_volume_value: Label
+@export var _music_volume_slider: HSlider
+@export var _music_volume_value: Label
+
+
+# The settings when the menu is opened
+var _old_audio_settings: Dictionary
+# The settigns that are changed in the menu
+# Is compared with the old settings to enable/disable the apply button or discard changes
+var _new_audio_settings: Dictionary
+
+
+func _setup_audio_settings() -> void:
+	var audio_settings: Dictionary = SettingsManager.load_audio_settings()
+	_old_audio_settings = audio_settings.duplicate()
+	_new_audio_settings = audio_settings.duplicate()
+	
+	# Master volume
+	_master_volume_slider.value = audio_settings.master_volume
+	_master_volume_slider.value_changed.connect(_on_master_volume_changed)
+	_master_volume_value.text = str(audio_settings.master_volume)
+	
+	# Effects volume
+	_effects_volume_slider.value = audio_settings.effects_volume
+	#_effects_volume_slider.value_changed.connect(_on_effects_volume_changed)
+	_effects_volume_value.text = str(audio_settings.effects_volume)
+	
+	# Music
+	_music_volume_slider.value = audio_settings.music_volume
+	#_music_volume_slider.value_changed.connect(_on_music_volume_changed)
+	_music_volume_value.text = str(audio_settings.music_volume)
+
+
+func _on_master_volume_changed(value: float) -> void:
+	_new_audio_settings.master_volume = value
+	_master_volume_slider.value = value
+	_master_volume_value.text = str(value)
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
+	
+	# Disable the apply button if the new and old values aren't matching
+	_apply_button.disabled = _are_new_and_old_settings_matching()
+
+#endregion
+
+
+
+#region Video settings
+
+@export_group("Video")
+@export var _display_mode_options_button: OptionButton
+@export var _vsync_mode_options_button: OptionButton
+@export var _fps_slider: HSlider
+@export var _fps_value: Label
+
+# The settings when the menu is opened
+var _old_video_settings: Dictionary
+# The settigns that are changed in the menu
+# Is compared with the old settings to enable/disable the apply button or discard changes
+var _new_video_settings: Dictionary
+
+
+func _setup_video_settings() -> void:
+	var video_settings: Dictionary = SettingsManager.load_video_settings()
+	_old_video_settings = video_settings.duplicate()
+	_new_video_settings = video_settings.duplicate()
+	
+	# Display mode
+	_display_mode_options_button.selected = video_settings.display_mode
+	_display_mode_options_button.item_selected.connect(_on_display_mode_changed)
+	
+	#VSync
+	_vsync_mode_options_button.selected = video_settings.vsync_mode
+	_vsync_mode_options_button.item_selected.connect(_on_vsync_mode_changed)
+	
+	# Frame rate
+	var vsync_enabled: bool = _vsync_mode_options_button.selected == DisplayServer.VSYNC_ENABLED
+	_fps_slider.editable = not vsync_enabled
+	_fps_slider.value = video_settings.max_fps
+	_fps_slider.value_changed.connect(_on_fps_changed)
+	
+	_fps_value.text = str(video_settings.max_fps)
+
+
+func _on_display_mode_changed(index: int) -> void:
+	_new_video_settings.display_mode = index
+	SettingsManager.set_display_mode(index)
+	
+	# Disable the apply button if the new and old values aren't matching
+	_apply_button.disabled = _are_new_and_old_settings_matching()
+
+
+func _on_vsync_mode_changed(index: int) -> void:
+	_new_video_settings.vsync_mode = index
+	SettingsManager.set_vsync_mode(index as DisplayServer.VSyncMode)
+	# Disable/Enable fps options if vsync is enabled
+	if index == DisplayServer.VSYNC_ENABLED:
+		_on_fps_changed(DisplayServer.screen_get_refresh_rate())
+		_fps_slider.editable = false
+	else:
+		_fps_slider.editable = true
+	
+	# Disable the apply button if the new and old values aren't matching
+	_apply_button.disabled = _are_new_and_old_settings_matching()
+
+
+func _on_fps_changed(value: float) -> void:
+	_new_video_settings.max_fps = value
+	_fps_slider.value = value
+	_fps_value.text = str(value)
+	
+	# Disable the apply button if the new and old values aren't matching
+	_apply_button.disabled = _are_new_and_old_settings_matching()
+
+#endregion
+
+
+var _old_gameplay_settings: Dictionary
+# The settigns that are changed in the menu
+# Is compared with the old settings to enable/disable the apply button or discard changes
+var _new_gameplay_settings: Dictionary
+
+
+func _setup_gameplay_settings() -> void:
+	pass
